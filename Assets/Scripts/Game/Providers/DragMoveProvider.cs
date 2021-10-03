0692c2213e6d3fee39e98a2e78e5a4b2ebdfcadc
OMOH98 <kosia4enko.g@gmail.com>
2021-10-04 01:22:23 +0300

    Rework DragMove again

--- Assets/Scripts/Game/Providers/DragMoveProvider.cs
+++ Assets/Scripts/Game/Providers/DragMoveProvider.cs
@@ -5,41 +5,39 @@ using UnityEngine;
 public class DragMoveProvider : RotationProvider
 {
     public float maxSensitivity = 1;
-    public float minSensitivity = 0.05f;
-    public AnimationCurve sensitivityIncrSpeed;
-    public AnimationCurve sensitivityFadeSpeed;
+    public float maxLength = 400;
+    public AnimationCurve sensitivityWrtLength;
 
     Vector2 diffAngles = Vector2.zero;
     Vector3 prevPos = Vector3.zero;
-    private float sensitivity;
+    Vector3 initialPos = Vector3.zero;
+
 
     protected new void Start()
     {
         base.Start();
-        sensitivity = minSensitivity;
     }
     void Update()
     {
         if (!readyToUpdate) return;
         if (Input.GetMouseButtonDown(0))
         {
-            prevPos = Input.mousePosition;
+            initialPos = Input.mousePosition;
         }
         if (Input.GetMouseButton(0))
         {
+            var sensitivity = sensitivityWrtLength.Evaluate((Input.mousePosition - initialPos).magnitude/maxLength) * maxSensitivity;
             var x = (-(prevPos.y - Input.mousePosition.y) * sensitivity);
             var y = (prevPos.x - Input.mousePosition.x) * sensitivity;
             x = Mathf.Clamp(x, -1, 1);
             y = Mathf.Clamp(y, -1, 1);
             diffAngles.Set(x, y);
-            sensitivity += Mathf.Clamp01(diffAngles.sqrMagnitude) * sensitivityIncrSpeed.Evaluate(sensitivity) * Time.deltaTime;
             prevPos = Input.mousePosition;
         }
         else
         {
             diffAngles.Set(0, 0);
         }
-        sensitivity = Mathf.Clamp(sensitivity - sensitivityFadeSpeed.Evaluate(sensitivity) * Time.deltaTime * (1 - Mathf.Clamp01(diffAngles.sqrMagnitude)), minSensitivity, maxSensitivity);
     }
 
     public override Vector2 GetAngles()
